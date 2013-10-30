9d3b4e6023550213607d01ad38ef7c0f5207c920
Jace Ju <jaceju@kkbox.com>
2013-10-30 10:56:09 +0800

    取得每一個需要操作的元素

--- js/calculator.js
+++ js/calculator.js
@@ -2,4 +2,18 @@
     var $ = function (id) {
         return d.getElementById(id);
     }
+    var screen = $('screen');
+    var btnMemClear = $('mem-clear');
+    var btnMemAdd = $('mem-add');
+    var btnMemSub = $('mem-sub');
+    var btnMemRemember = $('mem-remember');
+    var btnClear = $('clear');
+    var btnSign = $('sign');
+    var btnMathAdd = $('math-add');
+    var btnMathSub = $('math-sub');
+    var btnMathDiv = $('math-div');
+    var btnMathMul = $('math-mul');
+    var btnCalculate = $('calculate');
+    var btnNumbers = {};
+
 })(document);
