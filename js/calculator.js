2cae1f1c12a271c525c31ed951d1cb695b4a8f16
Jace Ju <jaceju@gmail.com>
2013-11-17 16:55:02 +0800

    加入註解

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,8 @@
     var $ = function (id) {
         return d.getElementById(id);
     }
+
+    // 取得顯示幕
     var screen = $('screen');
     var btnMemClear = $('mem-clear');
     var btnMemAdd = $('mem-add');
@@ -17,10 +19,12 @@
     var btnDot = $('dot');
     var btnNumbers = {};
 
+    // 取得數字按鈕
     for (var i = 0; i <= 9; i++) {
         btnNumbers[i] = $('num-' + i);
     }
 
+    // 按下數字按鈕
     for (var i = 0; i <= 9; i++) {
         btnNumbers[i].onclick = function () {
             if (screen.innerHTML === '0') {
