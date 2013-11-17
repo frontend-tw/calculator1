f6c253ba1fab5bf99474050631cedd4335282374
Jace Ju <jaceju@gmail.com>
2013-11-17 16:55:41 +0800

    重寫初始化程式碼

--- js/calculator.js
+++ js/calculator.js
@@ -5,19 +5,7 @@
 
     // 取得顯示幕
     var screen = $('screen');
-    var btnMemClear = $('mem-clear');
-    var btnMemAdd = $('mem-add');
-    var btnMemSub = $('mem-sub');
-    var btnMemRemember = $('mem-remember');
-    var btnClear = $('clear');
-    var btnSign = $('sign');
-    var btnMathAdd = $('math-add');
-    var btnMathSub = $('math-sub');
-    var btnMathDiv = $('math-div');
-    var btnMathMul = $('math-mul');
-    var btnCalculate = $('calculate');
-    var btnDot = $('dot');
-    var btnNumbers = {};
+    var btnNumbers = [];
 
     // 取得數字按鈕
     for (var i = 0; i <= 9; i++) {
